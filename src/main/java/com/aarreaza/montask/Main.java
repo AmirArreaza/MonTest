56d2f8d6b3e34a2db1d512520e0fe16d1c1e0026
AmirArreaza <ajaarreazav@gmail.com>
2019-11-07 13:30:48 +0000

    Task 004: Task services
    - Setting the main class

--- src/main/java/com/aarreaza/montask/Main.java
+++ src/main/java/com/aarreaza/montask/Main.java
@@ -1,15 +1,21 @@
 package com.aarreaza.montask;
 
 import com.aarreaza.montask.service.RESTServer;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.sql.DataSource;
 
-public class Main {
+@SpringBootApplication
+public class Main implements CommandLineRunner {
 
     public  static void main(String[] args){
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
         RESTServer server = new RESTServer();
         server.run();
-
     }
 }
