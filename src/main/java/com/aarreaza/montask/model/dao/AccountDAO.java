468d3a04cd8737dcfccf771bb3c88a9477dfc8e2
AmirArreaza <ajaarreazav@gmail.com>
2019-11-07 12:57:39 +0000

    Task 004: Task services
    - Removed unused import

--- src/main/java/com/aarreaza/montask/model/dao/AccountDAO.java
+++ src/main/java/com/aarreaza/montask/model/dao/AccountDAO.java
@@ -2,8 +2,6 @@ package com.aarreaza.montask.model.dao;
 
 import com.aarreaza.montask.model.Account;
 
-import java.util.List;
-
 public interface AccountDAO extends GenericDAO<Account> {
 
     operationResult updateBalance(Account account);
